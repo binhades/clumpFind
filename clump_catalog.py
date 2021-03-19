fix units problems Jy/beam -> mJy/pix

clump_catalog.py
@@ -18,11 +18,18 @@ def main(args):
     hdu = fits.open(args.fits_file)[0]
     hdr = hdu.header
     wcs = WCS(header=hdr).celestial
-    data = hdu.data * 0.05764 * 1000 # unit convert: Jy/beam -> mJy/pixel
+    data = hdu.data
     nchan = data.shape[0]
     velo = (np.arange(nchan) - hdr['CRPIX3'] + 1) * hdr['CDELT3'] + hdr['CRVAL3']
     ny = data.shape[1]
     nx = data.shape[2]
+
+    # unit convert: Jy/beam -> mJy/pixel
+    beam=4.7 # arcmin
+    pix = 1.0 # arcmin
+    pix_over_beam = pix**2/((beam/2)**2*np.pi)
+    print(pix_over_beam)
+    data = data * 1000 * pix_over_beam # x Jy/beam = (x * pix/beam) Jy/pix 
 
     # -------------------------------------------
     #    Load dendrogram
@@ -93,8 +100,6 @@ def main(args):
             writer.writerow(row)
     if args.file_csv is not None:
         fcsv.close()
-
-
 
     return 0
 

clump_plot.py
@@ -84,8 +84,8 @@ def plot_struc(fig, file_out,struc,velo,wcs,hdr,data,nx,ny,nchan,cmap,leaf_label
     md = np.ma.masked_array(data,mask=mask3d)
     spm = md.mean(axis=(1,2))
     spp = data[:,y_p,x_p]
-    spmsm = smooth(spm,window_len=5) * 1000.
-    sppsm = smooth(spp,window_len=5) * 1000.
+    spmsm = smooth(spm,window_len=5)
+    sppsm = smooth(spp,window_len=5)
 
     plot_spec(fig,velo,spmsm,sppsm,vline=v_p,title=title0)
     plot_imag(fig,imag,mask2d,wcs,coor=(x_p,y_p),title=title1,cmap=cmap)
@@ -117,6 +117,12 @@ def main(args):
     velo = (np.arange(nchan) - hdr['CRPIX3'] + 1) * hdr['CDELT3'] + hdr['CRVAL3']
     ny = data.shape[1]
     nx = data.shape[2]
+
+    # unit convert: Jy/beam -> mJy/pix
+    beam = 4.7 # arcmin
+    pix = 1.0 # arcmin
+    pix_over_beam = pix**2/((beam/2)**2*np.pi)
+    data = data * 1000 * pix_over_beam # x Jy/beam = (x * pix/beam) Jy/pix
 
     #------------------------
     #    Load dendrogram