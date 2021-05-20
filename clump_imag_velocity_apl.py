4ba7dcbd59d861cfa69cc61278ba414cb1efb12d
binhades <binhades@users.noreply.github.com>
2021-05-20 10:55:32 +0800

    plot with CRRL regions markers.

--- clump_imag_velocity_apl.py
+++ clump_imag_velocity_apl.py
@@ -3,36 +3,27 @@
 # to only create the overall image with leaves and branches on m0 map
 # choose to overlay sources in csv catalog
 
-import numpy as np
+import argparse, os, time, copy
 import aplpy
-from astropy.coordinates import Galactic
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import argparse, os
-from toolkit import colormap
-
+import numpy as np
 import matplotlib as mpl
+import matplotlib.pyplot as plt
+from astropy.coordinates import SkyCoord, Galactic
+from astropy.io import fits
+#from astropy import units as u
+from astrodendro import Dendrogram
+#from toolkit import smooth
+
 #mpl.rcParams['xtick.labelsize']='xx-large'
 #mpl.rcParams['ytick.labelsize']='xx-large'
 
-import argparse, os, time
 
-from toolkit import smooth
-from toolkit import colormap
-from astropy.io import fits
-from astropy.wcs import WCS
-from astropy.visualization import simple_norm
-from astropy import units as u
-from astropy.coordinates import SkyCoord
-from astrodendro import Dendrogram, ppv_catalog
-from matplotlib import pyplot as plt
-from matplotlib import cm
-import matplotlib.patches as patches
-from matplotlib.colors import LogNorm
-
-def plot_image(file_in,file_out=None,file_contour=None,file_reg=None,resize=None,contour=False,levels=None,contour_color='#d0d0d0',plot=False,oformat='png',skycoor='equ',vmin=None,vmax=None,pmin=0.25,pmax=99.75,stretch='linear',vmid=None,exponent=2,cmap='hot',beam=None,colorbar=None,dendro=None,file_catalog=None,addgal=True,save=True):
-    fig = aplpy.FITSFigure(file_in)
-    fig.show_colorscale(vmin=vmin,vmax=vmax,pmin=pmin,pmax=pmax,cmap=colormap(cmap),aspect='equal',smooth=1,stretch=stretch,vmid=vmid,exponent=exponent)
+def plot_image(file_fits,file_velo,file_out=None,file_sour=None,file_contour=None,file_reg=None,\
+               resize=None,contour=False,levels=None,contour_color='#d0d0d0',plot=False,oformat='png',\
+               skycoor='equ',vmin=None,vmax=None,pmin=0.25,pmax=99.75,stretch='linear',vmid=None,\
+               exponent=2,beam=None,colorbar=None,dendro=None,addgal=True,save=True):
+    fig = aplpy.FITSFigure(file_fits)
+    fig.show_colorscale(vmin=vmin,vmax=vmax,pmin=pmin,pmax=pmax,aspect='equal',smooth=1,stretch=stretch,vmid=vmid,exponent=exponent)
     fig.hide_colorscale()
 
 #    fig.hide_colorscale()
@@ -94,7 +85,6 @@ def plot_image(file_in,file_out=None,file_contour=None,file_reg=None,resize=None
     fig.ticks.set_length(2)
     fig.ticks.set_color('black')
     fig.ticks.show()
-    fig.set_nan_color('white')
     if colorbar is not None:
         fig.add_colorbar()
         fig.colorbar.show()
@@ -112,7 +102,7 @@ def plot_image(file_in,file_out=None,file_contour=None,file_reg=None,resize=None
     if contour:
         if levels is not None:
             if file_contour is None:
-                file_contour = file_in
+                file_contour = file_fits
             fig.show_contour(file_contour,levels=levels,smooth=1,colors=contour_color, linewidths=1)
         else:
             print("contour levels is not set")
@@ -120,8 +110,6 @@ def plot_image(file_in,file_out=None,file_contour=None,file_reg=None,resize=None
 #------------------------
 # Plot all leaves and branches on full frame of Moment0 map.
 #------------------------
-    # add sources in catalog
-#    add_source(ax0,catalog,wcs)
     # ------------------------
     # leaf label
     list_idx = [] # raw index
@@ -136,46 +124,53 @@ def plot_image(file_in,file_out=None,file_contour=None,file_reg=None,resize=None
     # ------------------------
 
     colors = ['blue','red','green']
-    v_cmap = mpl.cm.get_cmap('viridis')
-    v_cmap = mpl.cm.get_cmap('plasma')
-    v_cmap = mpl.cm.get_cmap('cividis')
-    v_cmap = mpl.cm.get_cmap('seismic')
-    v_cmap = mpl.cm.get_cmap('Reds')
-    v_cmap = mpl.cm.get_cmap('coolwarm')
-    catalog = load_csv(file_catalog)
-    add_source_velo(fig,catalog)
+    v_cmap = copy.copy(mpl.cm.get_cmap('viridis'))
+    v_cmap = copy.copy(mpl.cm.get_cmap('plasma'))
+    v_cmap = copy.copy(mpl.cm.get_cmap('cividis'))
+    v_cmap = copy.copy(mpl.cm.get_cmap('seismic'))
+    v_cmap = copy.copy(mpl.cm.get_cmap('Reds'))
+    v_cmap = copy.copy(mpl.cm.get_cmap('coolwarm'))
+
+
+    cata_velo = load_csv(file_velo)
+    add_source_velo(fig,cata_velo)
     norm = mpl.colors.Normalize(vmin=45,vmax=60)
-    c=1
+    count_branch=0
+
     for i, struc in enumerate(dendro.trunk):
         if struc.is_leaf:
             leaf_label = add_leaf_label(fig,leaves_idx_arr,struc,draw=False)
-            leaf_velo = get_source_velo(leaf_label,catalog)
+            leaf_velo = get_source_velo(leaf_label,cata_velo)
             color = colors[0]
             subtree = []
             line = 'solid'
             mask = struc.get_mask().mean(axis=0)
             if leaf_label == 19:
                 continue
-            fig.show_contour(mask,linewidths=1.5,levels=[0.001,1000],alpha=0.95,filled=True,colors=[v_cmap(norm(leaf_velo))],linestyles=line)#,smooth=1)
+            fig.show_contour(mask,levels=[0.001,1000],alpha=0.95,filled=True,colors=[v_cmap(norm(leaf_velo))],linestyles=line)#,smooth=1)
         elif struc.is_branch: # branch
-            color=colors[c]
-            c=c+1
+            count_branch=count_branch+1
+            color=colors[count_branch]
             subtree = struc.descendants
             line = 'dashed'
 
         for j, sub_struc in enumerate(subtree):
             if sub_struc.is_leaf:
                 leaf_label = add_leaf_label(fig,leaves_idx_arr,sub_struc,draw=False)
-                leaf_velo = get_source_velo(leaf_label,catalog)
+                leaf_velo = get_source_velo(leaf_label,cata_velo)
                 if leaf_label == 14 or leaf_label == 15:
                     continue
                 mask = sub_struc.get_mask().mean(axis=0)
-                fig.show_contour(mask,linewidths=1.5,levels=[0.001,1000],alpha=0.95,filled=True,colors=[v_cmap(norm(leaf_velo))])#,smooth=1)
+                fig.show_contour(mask,levels=[0.001,1000],alpha=0.95,filled=True,colors=[v_cmap(norm(leaf_velo))])#,smooth=1)
+
+    # add sources in catalog
+    if file_sour is not None:
+        add_source(fig,file_sour,v_cmap,norm)
 
 
     if save:
         if file_out is None:
-            file_out='m0-clumps_{}.png'.format(cmap)
+            file_out='clumps_velo_dist.png'
         fig.save(file_out,dpi=300,format=oformat,adjust_bbox=True)
 
     if plot:
@@ -214,6 +209,22 @@ def get_source_velo(ind0, catalog):
     return -1
 
 
+def add_source(fig, file_catalog,cmap,norm):
+    catalog = load_csv(file_catalog)
+    for item in catalog:
+        l = item['GLon']
+        b = item['GLat']
+        v = item['VLSR']
+        ind = item['Index']
+        if ind == 3:
+            offset = -0.03
+        else:
+            offset = 0.03
+        pos = SkyCoord(l,b,frame=Galactic,unit='deg')
+        #fig.show_markers(pos.icrs.ra.value, pos.icrs.dec.value,marker='o',s=200,facecolor=cmap(norm(v)))
+        fig.show_markers(pos.icrs.ra.value, pos.icrs.dec.value,marker='o',s=100,edgecolor='gray',facecolor='green')
+        fig.add_label(pos.icrs.ra.value, pos.icrs.dec.value+offset,'{:4.1f}'.format(v),fontsize=12,color='green')
+
 def add_source_velo(fig, catalog):
     for item in catalog:
         l = item['GLon']
@@ -232,26 +243,26 @@ def main(args):
     #    Load dendrogram
     #------------------------
     print('Load Dendrogram')
-    d = Dendrogram.load_from(args.file_d+'.hdf5')
- 
-    plot_image(args.file_map,args.file_out,file_reg=args.file_reg,file_contour=args.file_contour,\
-            plot=args.plot,oformat=args.format,resize=args.resize,\
+    d = Dendrogram.load_from(args.file_dend+'.hdf5')
+    print('')
+    plot_image(args.file_map,args.file_velo,file_out=args.file_out,file_reg=args.file_reg,file_contour=args.file_contour,\
+            plot=args.plot,save=args.save,oformat=args.format,resize=args.resize,\
             contour=args.contour,contour_color=args.contour_color,\
             levels=args.levels,skycoor=args.skycoor,beam=args.beam,\
             vmin=args.vmin,vmax=args.vmax,pmin=args.pmin,pmax=args.pmax,\
-            stretch=args.stretch,cmap=args.cmap,colorbar=args.colorbar,\
-            dendro=d,file_catalog=args.file_catalog)
+            stretch=args.stretch,\
+            dendro=d,file_sour=args.file_sour)
 
     return 0
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('file_map', type=str, help='the input data file')
-    parser.add_argument('--file_d', type=str, default='my_dendrogram', help='the dendrogram file')
-    parser.add_argument('--file_catalog', type=str, help='the catalog file to add source on map')
+    parser.add_argument('file_velo', type=str, help='the catalog file to add source on map')
+    parser.add_argument('file_dend', type=str, default='my_dendrogram', help='the dendrogram file')
+    parser.add_argument('--file_sour', type=str, help='the catalog file to add source on map')
     parser.add_argument('--chan_0', type=int, default=0,  help='channel index start')
     parser.add_argument('--chan_1', type=int, default=-1, help='channel index end')
-    parser.add_argument('--cmap', type=str, default='hot', help='the colormap, batlow or lajolla')
     parser.add_argument('--beam', type=float, help='set to add beam to image, size in pixel. 4.7 for FAST rrl cube')
     parser.add_argument('--proj', type=str, default='equ', help='equ or gal, projection of the map coordinate system')
 
@@ -265,6 +276,7 @@ if __name__ == '__main__':
     parser.add_argument('--levels', nargs='+', type=float, help='set contour levels')
     parser.add_argument('--contour_color', type=str, default='#d0d0d0', help='contour color')
     parser.add_argument('--plot', action='store_true', help='set to show plot')
+    parser.add_argument('--save', action='store_true', help='set to save plot')
     parser.add_argument('--vmin', type=float, help='Minimum pixel value to use for the colorscale.')
     parser.add_argument('--vmax', type=float, help='Maximum pixel value to use for the colorscale.')
     parser.add_argument('--colorbar', type=str, help='the colorbar title if is to show')
